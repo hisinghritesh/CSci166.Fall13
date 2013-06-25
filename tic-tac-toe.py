127ef95dab64b474c96c47cae2d1e5800b02b809
David C. Ruby <everestso@aol.com>
2013-06-25 14:55:12 -0700

    Tic-Tac-Toe (X-Only)

---
+++ tic-tac-toe.py
@@ -0,0 +1,111 @@
+"""Games, or Adversarial Search. (Chapters 6)
+
+"""
+
+from utils import *
+import random
+
+from games import *
+
+
+class TicTacToeX(Game):
+    """Play TicTacToe on an h x v board, with Max (first player) playing 'X'.
+    A state has the player to move, a cached utility, a list of moves in
+    the form of a list of (x, y) positions, and a board, in the form of
+    a dict of {(x, y): Player} entries, where Player is 'X' or 'O'."""
+    def __init__(self, h=3, v=3, k=3):
+        update(self, h=h, v=v, k=k)
+        moves = [(x, y) for x in range(1, h+1)
+                 for y in range(1, v+1)]
+        self.initial = Struct(to_move='X', utility=0, board={}, moves=moves)
+
+    def legal_moves(self, state):
+        "Legal moves are any square not yet taken."
+        return state.moves
+
+    def make_move(self, move, state):
+        if move not in state.moves:
+            return state # Illegal move has no effect
+        board = state.board.copy(); board[move] = state.to_move
+        moves = list(state.moves); moves.remove(move)
+        return Struct(to_move=if_(state.to_move == 'X', 'O', 'X'),
+                      utility=self.compute_utility(board, move, state.to_move),
+                      board=board, moves=moves)
+
+    def utility(self, state, player='X'):
+        "Return the value to X; 1 for win, -1 for loss, 0 otherwise."
+        return state.utility
+
+    def terminal_test(self, state):
+        "A state is terminal if it is won or there are no empty squares."
+        return state.utility != 0 or len(state.moves) == 0
+
+    def display(self, state):
+        board = state.board
+        for x in range(1, self.h+1):
+            for y in range(1, self.v+1):
+                print board.get((x, y), '.'),
+            print
+
+    def compute_utility(self, board, move, player):
+        "If X wins with this move, return 1; if O return -1; else return 0."
+        if (self.k_in_row(board, move, player, (0, 1)) or
+            self.k_in_row(board, move, player, (1, 0)) or
+            self.k_in_row(board, move, player, (1, -1)) or
+            self.k_in_row(board, move, player, (1, 1))):
+            return if_(player == 'X', +1, -1)
+        else:
+            return 0
+
+    def k_in_row(self, board, move, player, (delta_x, delta_y)):
+        "Return true if there is a line through move on board for player."
+        x, y = move
+        n = 0 # n is number of moves in row
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x + delta_x, y + delta_y
+        x, y = move
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x - delta_x, y - delta_y
+        n -= 1 # Because we counted move itself twice
+        return n >= self.k
+    
+def game_test():
+    game = TicTacToeX()
+    state = game.initial
+    print game
+    print state
+    player = game.to_move(state)
+    print player
+    utility = game.utility(state,player)
+    print utility
+    game.display(state)
+    print state
+    while not game.terminal_test(state):
+        move = minimax_decision(state, game)
+        print "move is:", move
+        state = game.make_move(move, state)
+        game.display(state)
+        print state
+        
+def playgame_test():
+    game = TicTacToeX()
+    state = game.initial
+    player = game.to_move(state)
+    utility = game.utility(state,player)
+    game.display(state)
+    ##move = minimax_decision(state, game)
+    ##print "move is:", move
+    while not game.terminal_test(state):
+        if state.to_move == 'O':
+            move = input("Enter move '(X,Y)': ")
+        else:
+            print "Computer searching for move..."
+            move = minimax_decision(state, game)
+        print "move is:", move
+        state = game.make_move(move, state)
+        game.display(state)
+        print state
+
+
